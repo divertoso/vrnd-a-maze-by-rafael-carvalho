0974e5afb2c055410c3724615c75d9e78f29990b
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-12-28 15:50:33 -0200

    Navigation Begin

---
+++ A Maze/Assets/UdacityVR/Scripts/Teste.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+/*
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
+        {
+
+            Debug.Log("cdddddddddddddddddddddddddd");
+        }
+        */
+    }
+
+
+    public void Clicou()
+    {
+        Debug.Log("chamou o metosdo");
+    }
+}
